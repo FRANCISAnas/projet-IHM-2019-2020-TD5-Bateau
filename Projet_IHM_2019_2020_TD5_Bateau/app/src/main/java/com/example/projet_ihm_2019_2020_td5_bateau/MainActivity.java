3e5c139db9fc0e5b6d49f2d40243c80e80cf3a26
FRANCISAnas <anas.francis@etu.univ-cotedazur.fr>
2020-03-26 01:11:21 +0100

    setting the repository ...

--- Projet_IHM_2019_2020_TD5_Bateau/app/src/main/java/com/example/projet_ihm_2019_2020_td5_bateau/MainActivity.java
+++ Projet_IHM_2019_2020_TD5_Bateau/app/src/main/java/com/example/projet_ihm_2019_2020_td5_bateau/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.projet_ihm_2019_2020_td5_bateau;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
