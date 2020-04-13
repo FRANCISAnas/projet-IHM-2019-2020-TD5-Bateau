b648463e5da724ce64d8bb6ac87cec15b38dcaa1
FRANCISAnas <anas.francis@etu.univ-cotedazur.fr>
2020-04-13 17:12:12 +0200

    spinners for setting fragments need some improvments

--- project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/Fragments/SettingsFragment.java
+++ project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/Fragments/SettingsFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.ihmprojet_2019_2020_td5_bateaux.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     public SettingsFragment(){
         // Empty constructor required
     }
@@ -18,7 +23,25 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-
+        Spinner spinner = rootView.findViewById(R.id.spinnerTemp);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.tempSpinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         return rootView;
+    }
+
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
