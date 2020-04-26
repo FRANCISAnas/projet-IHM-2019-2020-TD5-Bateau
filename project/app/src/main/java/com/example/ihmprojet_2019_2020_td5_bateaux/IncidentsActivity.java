ad56c28ab607c7505d13d8ee9bf1bcb594492909
BOAGLE251 <eyasu.bogale@etu.unice.fr>
2020-04-26 13:08:33 +0200

    Post Incident Service

--- project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/IncidentsActivity.java
+++ project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/IncidentsActivity.java
@@ -8,6 +8,7 @@ import android.widget.ListView;
 
 import com.example.ihmprojet_2019_2020_td5_bateaux.Metier.Incident;
 import com.example.ihmprojet_2019_2020_td5_bateaux.Metier.IncidentListAdapter;
+import com.example.ihmprojet_2019_2020_td5_bateaux.Service.PostService;
 import com.example.ihmprojet_2019_2020_td5_bateaux.Service.Service;
 
 import java.util.ArrayList;
@@ -22,10 +23,12 @@ public class IncidentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incidents);
 
-        ListView listView = (ListView) findViewById(R.id.myListView);
-        Service incidentService = new Service(this,listView);
-        incidentService.execute();
+        //ListView listView = (ListView) findViewById(R.id.myListView);
+        //Service incidentService = new Service(this,listView); //getWindow().getDecorView().getRootView()
+        //incidentService.execute();
 
+        PostService service = new PostService(this);
+        service.execute();
 
 
 
