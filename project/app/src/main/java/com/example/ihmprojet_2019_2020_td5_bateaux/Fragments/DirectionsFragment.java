290052fe054807971c7c2e30d690d545c187ee14
safaelbou <46226042+safaelbou@users.noreply.github.com>
2020-05-01 19:05:32 +0200

    implementation open street map

--- project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/Fragments/DirectionsFragment.java
+++ project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/Fragments/DirectionsFragment.java
@@ -1,12 +1,15 @@
 package com.example.ihmprojet_2019_2020_td5_bateaux.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.ihmprojet_2019_2020_td5_bateaux.DirectionsActivity;
 import com.example.ihmprojet_2019_2020_td5_bateaux.R;
 
 public class DirectionsFragment extends Fragment {
@@ -18,7 +21,14 @@ public class DirectionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
         View rootView = inflater.inflate(R.layout.fragment_directions, container, false);
-
+        Button btn =(Button) rootView.findViewById( R.id.go_to_Directions_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in =new Intent(getActivity(), DirectionsActivity.class);
+                startActivity(in);
+            }
+        });
         return rootView;
     }
 }
