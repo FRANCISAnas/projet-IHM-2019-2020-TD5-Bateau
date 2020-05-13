98dbd143351301a668fe05c68e74f5c9879b97fc
Kevin Lompo <kevinlompo12@gmail.com>
2020-05-13 14:56:27 +0200

    optimisation affichage dans le fragment about et settings

--- project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/Fragments/SettingsFragment.java
+++ project/app/src/main/java/com/example/ihmprojet_2019_2020_td5_bateaux/Fragments/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.example.ihmprojet_2019_2020_td5_bateaux.Fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,26 +20,37 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
+import com.example.ihmprojet_2019_2020_td5_bateaux.HomeActivity;
 import com.example.ihmprojet_2019_2020_td5_bateaux.MainActivity;
 import com.example.ihmprojet_2019_2020_td5_bateaux.R;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.Locale;
+import java.util.prefs.AbstractPreferences;
 
 public class SettingsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     public static boolean URGENT_NOTIFICATIONS = false;
     Locale myLocale;
-    String currentLanguage = "en", currentLang;
+    private String currentLanguage = "en";
+    private String currentLang;
     private CheckBox checkBox;
+    int etat;
+    private String Tag = "kevin";
+    public SharedPreferences.Editor editor;
+    private AbstractPreferences prefEdit;
+    public SharedPreferences prefs;
 
     public SettingsFragment() {
         // Empty constructor required
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState) {
 
-        currentLanguage = getIntent().getStringExtra(currentLang);
+        //currentLanguage = getIntent().getStringExtra(currentLang);
 
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
         Spinner spinner = rootView.findViewById(R.id.spinnerTemp);
@@ -51,6 +66,7 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
         spinner1.setAdapter(adapter1);
         spinner1.getBackground().setColorFilter(getResources().getColor(R.color.design_default_color_primary_dark), PorterDuff.Mode.SRC_ATOP);
         spinner1.setOnItemSelectedListener(this);
+
 
 
         checkBox = (CheckBox) rootView.findViewById(R.id.checkBox);
@@ -95,33 +111,42 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
         // String text = parent.getItemAtPosition(position).toString();
         //  Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
 
+
         Configuration config;
         switch (pos) {
             case 0:
-                break;
-
-            case 1:
                 setLocale("en");
                 break;
 
-            case 2:
+            case 1:
                 setLocale("fr");
                 break;
+
         }
 
     }
 
+
+
     public void setLocale(String localeName) {
+
         if (!localeName.equals(currentLanguage)) {
             myLocale = new Locale(localeName);
             Resources res = getResources();
             DisplayMetrics dm = res.getDisplayMetrics();
             Configuration conf = res.getConfiguration();
-            conf.locale = myLocale;
+            if(Build.VERSION.SDK_INT >=25) {
+                conf.setLocale(myLocale);
+            } else{
+
+                conf.locale = myLocale;
+            }
+
             res.updateConfiguration(conf, dm);
-            Intent refresh = new Intent(getContext(), MainActivity.class);
-            refresh.putExtra(currentLang, localeName);
+            Intent refresh = new Intent(getContext(), HomeActivity.class);
+            refresh.putExtra(currentLanguage, localeName);
             startActivity(refresh);
+
         } else {
             Toast.makeText(getContext(), "Language already selected!", Toast.LENGTH_SHORT).show();
         }
